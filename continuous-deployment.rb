5388970fb1e90cbad89aea0dcbc31e824523a8e0
Thomas Gotwig <github@tomasu.mozmail.com>
2022-05-08 19:25:41 +0000

    continuous-deployment 0.0.3

--- continuous-deployment.rb
+++ continuous-deployment.rb
@@ -1,9 +1,9 @@
 class ContinuousDeployment < Formula
   desc "desc... :-)"
   homepage "https://github.com/TGotwig/continuous-deployment"
-  url "https://github.com/tgotwig/continuous-deployment/releases/download/0.0.2/continuous-deployment-linux.tar.gz"
-  version "0.0.2"
-  sha256 "de3de44b8b26bfa3b7680b63c495334aacd0607cc0ae351d0e58bf2ef7a7f715"
+  url "https://github.com/tgotwig/continuous-deployment/releases/download/0.0.3/continuous-deployment-linux.tar.gz"
+  version "0.0.3"
+  sha256 "8b36c3c3ab5ffc12f57fc5264ecbda10acd1870c6dbb1712074c37c8cffc6811"
 
   def install
     bin.install "continuous-deployment"

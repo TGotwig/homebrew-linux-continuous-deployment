a94db9d4e55359b45ccef2aba049706d2e674a46
Thomas Gotwig <github@tomasu.mozmail.com>
2022-05-11 07:11:58 +0000

    continuous-delivery 0.0.5

--- continuous-delivery.rb
+++ continuous-delivery.rb
@@ -1,9 +1,9 @@
 class ContinuousDelivery < Formula
   desc "desc... :-)"
   homepage "https://github.com/TGotwig/continuous-delivery"
-  url "https://github.com/tgotwig/continuous-delivery/releases/download/0.0.3/continuous-delivery-linux.tar.gz"
-  version "0.0.3"
-  sha256 "8b36c3c3ab5ffc12f57fc5264ecbda10acd1870c6dbb1712074c37c8cffc6811"
+  url "https://github.com/tgotwig/continuous-delivery/releases/download/0.0.5/continuous-delivery-linux.tar.gz"
+  version "0.0.5"
+  sha256 "02bce868aef2a4a034485845fc4ad6d75892006bd098e75bc57c7998cfe294b2"
 
   def install
     bin.install "continuous-delivery"
